46ae6bc55433372b917bf3f1ca5f260cb492b49b
duanlang <1075095689@qq.com>
2022-03-14 11:47:44 +0800

    🎉 初始化项目

---
+++ index.js
@@ -0,0 +1,9 @@
+/**
+ * @version: v1.0.0
+ * @author: xiaoqiujun
+ * @Github: https://github.com/xiaoqiujun
+ * @date: Do not edit
+ * @LastEditors: xiaoqiujun
+ * @LastEditTime: 2022-03-14 11:47:02
+ * @FilePath: \linsql\index.js
+ */
