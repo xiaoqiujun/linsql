79bde9aa70bb0e23a231b0120f9d1b83b2e4d44f
duanlang <1075095689@qq.com>
2022-03-23 18:50:11 +0800

    ✨ 优化打包

--- rollup.config.js
+++ rollup.config.js
@@ -4,35 +4,43 @@ import commonjs from "rollup-plugin-commonjs";
 import typescript from "rollup-plugin-typescript2";
 import json from "@rollup/plugin-json";
 import babel from "rollup-plugin-babel";
-import {name,version} from './package.json'
-import path from 'path'
-const file = type => `./${name}.${type}.js`
+import { name, version,dependencies,peerDependencies} from "./package.json";
+const file = (type) => `./${name}.${type}.js`;
 const banner =
-  '/*\n' +
-  ` * linsql v${version}\n` +
-  ` * (c) 2022-${new Date().getFullYear()} xiaoqiujun\n` +
-  ' * Released under the MIT License.\n' +
-  ' */';
+	"/*\n" +
+	` * linsql v${version}\n` +
+	` * (c) 2022-${new Date().getFullYear()} xiaoqiujun\n` +
+	" * Released under the MIT License.\n" +
+	" */";
 export default {
 	input: "./src/index.ts",
 	output: {
 		// 打包出口
 		file: "./dist/index.js",
-        name:name,
-		format:"umd",
-		banner
+		name: name,
+		format: "cjs",
+		exports:'named',
+		banner,
 	},
+	external: [	//需要配置这个,否则会报 / (!) Unresolved dependencies/相关的错误
+		...Object.keys(dependencies || {}),
+		...Object.keys(peerDependencies || {}),
+	],
 	plugins: [
 		// 打包插件
-		resolve(),
+		resolve({}),
 		commonjs(),
 		json(),
 		typescript({
-			tsconfig:path.resolve(__dirname, './tsconfig.json'),
-			extensions:['.js', '.ts']
+			tsconfigOverride: {
+				compilerOptions: {
+					module: "ES2015",
+				},
+			},
+			extensions: [".js", ".ts"],
 		}),
 		babel({
-			exclude: 'node_modules/**',
-		})
+			exclude: "node_modules/**",
+		}),
 	],
 };
